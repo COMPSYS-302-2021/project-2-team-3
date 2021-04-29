69de19668aaa3d92f9f5fbca516531385f13d246
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2021-04-30 10:54:15 +1200

    Added some comments

--- app/src/main/java/com/example/a302projecct2/RecViewAdapter.java
+++ app/src/main/java/com/example/a302projecct2/RecViewAdapter.java
@@ -1,5 +1,7 @@
 package com.example.a302projecct2;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -8,23 +10,37 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class RecViewAdapter extends RecyclerView.Adapter<RecViewAdapter.MyViewHolder> {
 
+    Context ctx;
 
+    public RecViewAdapter(Context ctx){
+        this.ctx = ctx;
+    }
+
+
+    //Inflating layout for each item within the RecyclerView
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(this.ctx);
+        //TODO: Create layout file for displaying each item
+//        View view = inflater.inflate(R.layout.my_row, parent, false);
+//        return new MyViewHolder(view);
         return null;
     }
 
+    //Binds the data to the layout that will be used for each item
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
     }
 
     @Override
+    //Used to return the number of items that would be displayed
     public int getItemCount() {
         return 0;
     }
 
+    //Connecting variables within here to components from the page
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
