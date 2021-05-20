d64b9d2365cf9870595a68ec846ed776a7bd9a58
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2021-05-20 22:08:05 +1200

    Completed ListActivity recyclerview adapter

---
+++ app/src/main/java/com/example/a302projecct2/ItemClass.java
@@ -0,0 +1,30 @@
+package com.example.a302projecct2;
+
+public class ItemClass {
+
+    String itemName, itemDescription, itemPrice;
+    String[] itemImages;
+
+    public ItemClass(String itemName, String itemDescription, String itemPrice, String[] itemImages) {
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemPrice = itemPrice;
+        this.itemImages = itemImages;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public String getItemPrice() {
+        return itemPrice;
+    }
+
+    public String[] getItemImages() {
+        return itemImages;
+    }
+}
--- app/src/main/java/com/example/a302projecct2/ListActivityRecAdapter.java
+++ app/src/main/java/com/example/a302projecct2/ListActivityRecAdapter.java
@@ -1,19 +1,28 @@
 package com.example.a302projecct2;
 
+import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 public class ListActivityRecAdapter extends RecyclerView.Adapter<ListActivityRecAdapter.MyViewHolder> {
 
     Context ctx;
+    ItemClass[] items; 
 
-    public ListActivityRecAdapter(Context ctx){
+    public ListActivityRecAdapter(Context ctx, ItemClass[] items){
         this.ctx = ctx;
+        this.items = items;
     }
 
 
@@ -22,28 +31,57 @@ public class ListActivityRecAdapter extends RecyclerView.Adapter<ListActivityRec
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(this.ctx);
-        //TODO: Create layout file for displaying each item
-//        View view = inflater.inflate(R.layout.my_row, parent, false);
-//        return new MyViewHolder(view);
-        return null;
+        View view = inflater.inflate(R.layout.list_item, parent, false);
+        return new MyViewHolder(view);
     }
 
     //Binds the data to the layout that will be used for each item
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.txtItemName.setText(items[position].getItemName());
+        holder.txtItemPrice.setText(items[position].getItemPrice());
+        Glide.with(ctx)
+                .load(items[position].getItemImages()[0])
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(holder.imgListItem);
+
+        holder.cvListItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ctx, viewItemPage.class);
+                intent.putExtra("itemName",items[position].getItemName());
+                intent.putExtra("itemPrice",items[position].getItemPrice());
+                intent.putExtra("itemDescription",items[position].getItemDescription());
+                ctx.startActivity(intent);
+            }
+        });
+
+
 
     }
 
     @Override
     //Used to return the number of items that would be displayed
     public int getItemCount() {
-        return 0;
+        return items.length;
     }
 
     //Connecting variables within here to components from the page
     public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtItemName, txtItemPrice;
+        ImageView imgListItem;
+        CardView cvListItem;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            txtItemName = itemView.findViewById(R.id.txtItemName);
+            txtItemPrice = itemView.findViewById(R.id.txtItemPrice);
+            imgListItem = itemView.findViewById(R.id.img_list_item);
+            cvListItem = itemView.findViewById(R.id.cvListItem);
+
+
         }
     }
 }
