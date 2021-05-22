29a7a273fd394eb72fd6335326cd17c1875a1fb5
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2021-05-22 18:10:34 +1200

    Created variable that contains information for categories recycler view

--- app/src/main/java/com/example/a302projecct2/dataprovider/DataProviderClass.java
+++ app/src/main/java/com/example/a302projecct2/dataprovider/DataProviderClass.java
@@ -5,7 +5,12 @@ public class DataProviderClass {
     CategoryClass[] categories;
     ItemClass[][] allItems;
 
-    public DataProviderClass(CategoryClass[] categories) {
+    public DataProviderClass() {
         //TODO: Assign values and create dataset for both categories and items
+        categories = new CategoryClass[]{
+                new CategoryClass("Japanese", "https://cdn.icon-icons.com/icons2/230/PNG/256/Japan_JP_JPN_392_Flag1_26102.png"),
+                new CategoryClass("Indian", "https://cdn.icon-icons.com/icons2/2087/PNG/512/india_icon_127891.png"),
+                new CategoryClass("Italian", "https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/256/Italy-Flag-icon.png")
+        };
     }
 }
