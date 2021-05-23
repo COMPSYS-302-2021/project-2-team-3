6616d03a8391d23c1018e4a1383eb6b92f6ffb46
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2021-05-24 09:51:16 +1200

    Added all japanese dishes information

--- app/src/main/java/com/example/a302projecct2/dataprovider/DataProviderClass.java
+++ app/src/main/java/com/example/a302projecct2/dataprovider/DataProviderClass.java
@@ -1,16 +1,54 @@
 package com.example.a302projecct2.dataprovider;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.a302projecct2.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class DataProviderClass {
 
-    CategoryClass[] categories;
-    ItemClass[][] allItems;
+    private CategoryClass[] cuisines;
+    //ItemClass[][] allItems;
+    private ArrayList<ItemClass[]> allDishes;
+
 
     public DataProviderClass() {
+
+        //Allows us to use getResources function
+        AppCompatActivity ac = new AppCompatActivity();
+
         //TODO: Assign values and create dataset for both categories and items
-        categories = new CategoryClass[]{
+        cuisines = new CategoryClass[]{
                 new CategoryClass("Japanese", "https://cdn.icon-icons.com/icons2/230/PNG/256/Japan_JP_JPN_392_Flag1_26102.png"),
                 new CategoryClass("Indian", "https://cdn.icon-icons.com/icons2/2087/PNG/512/india_icon_127891.png"),
                 new CategoryClass("Italian", "https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/256/Italy-Flag-icon.png")
         };
+
+
+        //Maybe try for loops to store data, need size of array for each item
+        String[] japaneseDishNames = ac.getResources().getStringArray(R.array.japanese_dishes);
+
+
+
+
+
+
+
+
+
+
     }
+
 }
+
+
+
+
+
