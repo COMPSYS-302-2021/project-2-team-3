2f767566486e2023d3e07c1eb622d714e4f7b8fd
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2021-05-25 13:41:55 +1200

    Added empty json file for storing data

--- app/src/main/java/com/example/a302projecct2/MainActivity.java
+++ app/src/main/java/com/example/a302projecct2/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.a302projecct2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] info = getResources().getStringArray(R.array.a);
+//                String item = info[0];
+                System.out.println(info[0]);
+                System.out.println(info[0].getClass().getSimpleName());
+            }
+        });
     }
 }
