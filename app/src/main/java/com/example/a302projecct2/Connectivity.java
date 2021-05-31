04dcbe46166fd4083b76679dc7613fb32f5d0d3d
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2021-06-01 11:31:34 +1200

    Created class for checking connectivity

---
+++ app/src/main/java/com/example/a302projecct2/Connectivity.java
@@ -0,0 +1,52 @@
+package com.example.a302projecct2;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.provider.Settings;
+
+public class Connectivity {
+
+    Context ctx;
+    public Connectivity(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    //Check if device is connected a network
+    private boolean isConnected(){
+        ConnectivityManager connectivityManager = (ConnectivityManager)ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //Check if device is connected to internet
+        NetworkInfo wifiConn = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileConn = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if((wifiConn != null && wifiConn.isConnected()) || (mobileConn != null && mobileConn.isConnected())){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    private void showNotConnectedDialog(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+        builder.setMessage("Please connect to the internet to proceed")
+                .setCancelable(false)
+                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ctx.startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                });
+        builder.show();
+
+
+    }
+
+
+}
