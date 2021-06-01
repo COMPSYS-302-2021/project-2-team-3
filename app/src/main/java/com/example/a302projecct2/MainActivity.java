386dc3cb66af597fcc602f84e1955839e606a95d
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2021-06-01 19:57:34 +1200

    added some code for loading screen

--- app/src/main/java/com/example/a302projecct2/MainActivity.java
+++ app/src/main/java/com/example/a302projecct2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a302projecct2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -9,18 +10,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME = 3000;
     private ImageView imgLoading;
+    private boolean connected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//
-//        Intent intent  = new Intent(this, Homepage.class);
-//        startActivity(intent);
-        imgLoading = findViewById(R.id.imgLoading);
-//        imgLoading.setImageResource(R.drawable.feastcuisine);
+        Connectivity conn = new Connectivity(getBaseContext());
+        while (!connected){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if(conn.isConnected()){
+                        connected = true;
+                        System.out.println("Connected to internet");
+                    }
+                }
+            }, SPLASH_TIME);
+        }
+        Intent intent = new Intent(this, Homepage.class);
+        startActivity(intent);
+
+
 
 
     }
